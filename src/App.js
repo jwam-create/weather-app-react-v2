b6ea04f9d8d26d2485e2b7f8319167b3af672790
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-02-05 14:26:10 -0500

    Integrated API with search, to display current weather conditions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Search from "./Search";
-import CurrentWeather from "./CurrentWeather";
+
 import Forecast from "./Forecast";
 import Footer from "./Footer";
 
 import "./App.css";
-import "./CurrentWeather.css";
+
 import "./Forecast.css";
 import "./Footer.css";
 
@@ -13,7 +13,6 @@ export default function App() {
 		<div className="App">
 			<div className="Container weather-app">
 				<Search />
-				<CurrentWeather />
 				<Forecast />
 				<Footer />
 			</div>
--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,69 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 import "./Search.css";
 
 export default function Search() {
-	const []
+	const [city, setCity] = useState("Paris");
+	const [currentWeather, setCurrentWeather] = useState(null);
+
+	function displayWeather(response) {
+		console.log(response.data);
+		let list = (
+			<div className="row">
+				<div className="col-6 city-container">
+					<h1 id="city">{city}</h1>
+					<ul>
+						<li>
+							<span id="description">{response.data.weather[0].main}</span>
+						</li>
+						<li>
+							Humidity:
+							<strong>
+								<span id="humidity"> {response.data.main.humidity}</span>%
+							</strong>
+							, Wind:{" "}
+							<strong>
+								<span id="wind">{Math.round(response.data.wind.speed)}</span>
+								km/h
+							</strong>
+						</li>
+						<li className="last-updated">
+							Last updated: <span id="date">Saturday 16:30</span>
+						</li>
+					</ul>
+				</div>
+				<div className="col">
+					<div className="temperature-container d-flex justify-content-end">
+						<img
+							src="images/fullsun.png"
+							alt=""
+							className="img-fluid"
+							id="icon"
+						/>
+						<div>
+							<span className="temperature" id="temperature">
+								{Math.round(response.data.main.temp)}
+							</span>
+							<span className="units">°C</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+		setCurrentWeather(list);
+	}
+
 	function handleSubmit(event) {
 		event.preventDefault();
+
+		let apiKey = "3ef1c4739274de1e0c3fc584c54fc2ec";
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3ef1c4739274de1e0c3fc584c54fc2ec`;
+		axios.get(apiUrl).then(displayWeather);
 	}
 
 	function handleSearchInput(event) {
-		console.log(event.target.value);
+		setCity(event.target.value);
 	}
 
 	return (
@@ -35,6 +89,7 @@ export default function Search() {
 					</div>
 				</div>
 			</form>
+			<div className="CurrentWeather">{currentWeather}</div>
 		</div>
 	);
 }
