77a4df2d67f0b0fc69815a35626df91626a64e35
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-02-05 13:59:19 -0500

    Added search functionality.

--- src/Search.js
+++ src/Search.js
@@ -3,26 +3,38 @@ import React from "react";
 import "./Search.css";
 
 export default function Search() {
+	const []
+	function handleSubmit(event) {
+		event.preventDefault();
+	}
+
+	function handleSearchInput(event) {
+		console.log(event.target.value);
+	}
+
 	return (
-		<form className="Search" id="search-input">
-			<div className="row">
-				<div className="col-9">
-					<input
-						type="search"
-						placeholder="Enter a city.."
-						className="form-control search-input"
-						id="city-input"
-						autoComplete="off"
-					/>
+		<div>
+			<form className="Search" id="search-input" onSubmit={handleSubmit}>
+				<div className="row">
+					<div className="col-9">
+						<input
+							type="search"
+							placeholder="Enter a city.."
+							className="form-control search-input"
+							id="city-input"
+							autoComplete="off"
+							onChange={handleSearchInput}
+						/>
+					</div>
+					<div className="col-3">
+						<input
+							type="submit"
+							className="btn btn-primary w-100"
+							value="Search"
+						/>
+					</div>
 				</div>
-				<div className="col-3">
-					<input
-						type="submit"
-						className="btn btn-primary w-100"
-						value="Search"
-					/>
-				</div>
-			</div>
-		</form>
+			</form>
+		</div>
 	);
 }
