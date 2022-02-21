4474701a166b2902291174a8c28c02deb64b2396
jwam-create <77247458+jwam-create@users.noreply.github.com>
2022-02-21 11:37:18 -0500

    Add forecast layout and styling

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,61 @@
 import React from "react";
 
 import "./Forecast.css";
+import WeatherIcon from "./WeatherIcon";
 
 export default function Forecast() {
 	return (
-		<div className="Forecast">
-			<p> Forecast placeholder </p>
+		<div className="Forecast row">
+			<div className="col">
+				<ul>
+					<li className="week-day">Mon</li>
+					<li>
+						<WeatherIcon code="01d" />
+					</li>
+					<li className="forecast-high">11°C</li>
+					<li>5°C</li>
+				</ul>
+			</div>
+			<div className="col">
+				<ul>
+					<li className="week-day">Tues</li>
+					<li>
+						<WeatherIcon code="01d" />
+					</li>
+					<li className="forecast-high">11°C</li>
+					<li>5°C</li>
+				</ul>
+			</div>
+			<div className="col">
+				<ul>
+					<li className="week-day">Wed</li>
+					<li>
+						<WeatherIcon code="01d" />
+					</li>
+					<li className="forecast-high">11°C</li>
+					<li>5°C</li>
+				</ul>
+			</div>
+			<div className="col">
+				<ul>
+					<li className="week-day">Thurs</li>
+					<li>
+						<WeatherIcon code="01d" />
+					</li>
+					<li className="forecast-high">11°C</li>
+					<li>5°C</li>
+				</ul>
+			</div>
+			<div className="col">
+				<ul>
+					<li className="week-day">Fri</li>
+					<li>
+						<WeatherIcon code="01d" />
+					</li>
+					<li className="forecast-high">11°C</li>
+					<li>5°C</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
